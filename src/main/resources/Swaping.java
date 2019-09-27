653f72cf824bdf5b6760a58b9d76fdfa6a0f3c2f
dev1 <m.lakshmikar23@gmail.com>
2019-09-28 01:53:41 +0530

    Jenkinsfile added

---
+++ src/main/resources/Swaping.java
@@ -0,0 +1,15 @@
+
+public class Swaping 
+{
+  public static void main(String[] args) 
+   {
+	  int a, b;
+	   a = 20;
+	   b = 30;
+	   System.out.println("Before swapping : a, b = "+a+", "+ + b);
+	    b=a+b;//50
+	    a=b-a;//30
+	    b=b-a;//20
+	  System.out.println("After swapping : a, b = "+a+", "+ + b);
+	 }
+	}
